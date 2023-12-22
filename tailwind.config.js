449dbbf1386fd1a2b43ef5afff00db1c7b175c98
AbuBokorprog <abubokor1066@gmail.com>
2023-12-22 11:04:27 +0600

    project initial

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#009db5",
+
+          secondary: "#009385",
+
+          accent: "#00a700",
+
+          neutral: "#36241d",
+
+          "base-100": "#fff8ff",
+
+          info: "#008ff3",
+
+          success: "#79eb26",
+
+          warning: "#b52c00",
+
+          error: "#c12442",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
